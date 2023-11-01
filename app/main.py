64c440247e70f866834ecb34b55e3c896cd60e7b
sifatnabil <sifatnabil@gmail.com>
2023-11-02 00:22:43 +0600

    Initial Commit

---
+++ app/main.py
@@ -0,0 +1,18 @@
+import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI(
+    title="Financial Analyzer APIs",
+    docs_url="/api/docs",
+    redoc_url="/api/redoc",
+    openapi_url="/api/openapi.json"
+)
+
+# app.include_router(transactions.router, prefix="/api")
+
+@app.get("/")
+async def root():
+    return {"message": "Successful Connection"}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
